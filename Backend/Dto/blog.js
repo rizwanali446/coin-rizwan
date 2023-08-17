c9dbcd59d16ddf79dac6562359103d7b141be7b4
rizwanali446 <60592207+rizwanali446@users.noreply.github.com>
2023-08-17 11:14:21 +0500

    improved blog dto

--- Backend/Dto/blog.js
+++ Backend/Dto/blog.js
@@ -2,7 +2,7 @@ class BlogDTO {
   constructor(blog) {
     this._id = blog._id;
     this.author = blog.author;
-    this.authorDetail = blog.authorDetail ?? null;
+    this.authorDetail = blog.authorDetail;
     this.content = blog.content;
     this.title = blog.title;
     this.photo = blog.photoPath;
